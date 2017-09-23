141516b26b48ad44fa596579d2ddd33605a7d940
fran <fgarcia.lopez@hotmail.com>
2017-09-23 15:49:25 -0600

    salvo history de clase 5 + ejemplo de su

--- src/tmp_script.sh
+++ src/tmp_script.sh
@@ -3,7 +3,7 @@
 GREEN="\033[0;32m"
 RED="\033[0;31m"
 YELLOW="\033[0;33m"
-RESET="033[0m"
+RESET="\033[0m"
  
 
 # revisar que el usuario no sea root
@@ -14,14 +14,13 @@ then
 	exit 1
 fi
 
-#obtencion de informaicion
+#obtencion de informacion
 
-echo -en "escriba su nombre completo >"
+echo -en "escriba su nombre completo > "
 read NOMBRE
   
 echo -en "escriba su correo electronico"
 read EMAIL
-
 
 echo -en "hola $NOMBRE, este es su correo [${GREEN}$EMAI${RESET}]? [s/n]: "
 read RESP
@@ -33,5 +32,10 @@ then
 	echo hola $NOMBRE, su correo es $EMAIL
 
 fi
+
+echo -en 
+sudo su - estudiantes -c "git config --global user.name \"$NOMBRE""
+sudo su - estudiantes -c "git config --global user.email \"$EMAIL""
+sudo su - estudiantes -c "git config --global core.editor vim"
 
 
