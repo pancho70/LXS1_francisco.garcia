b4d28bea5015826db487f619f07497304e5dcb8d
fran <fgarcia.lopez@hotmail.com>
2017-09-23 15:28:57 -0600

    Agrego solicitud de informacion del usuario

--- src/tmp_script.sh
+++ src/tmp_script.sh
@@ -2,20 +2,36 @@
 
 GREEN="\033[0;32m"
 RED="\033[0;31m"
-YELLOWN="\033[0;33m"
-RESET="033[0M"
+YELLOW="\033[0;33m"
+RESET="033[0m"
  
 
-
-
-# revisar que sea usuario root
-if [[ 0 != $UID ]]
+# revisar que el usuario no sea root
+if [[ 0 == $UID ]]
 then 
         #Inicnio elcodigo de color con red, luego los elimino con reset
-	echo -e ${RED}usted debe ser root para correr este script${RESET}"
+	echo -e ${RED}usted no debe root para correr este script${RESET}"
 	exit 1
 fi
 
-echo Hola $USER
+#obtencion de informaicion
+
+echo -en "escriba su nombre completo >"
+read NOMBRE
+  
+echo -en "escriba su correo electronico"
+read EMAIL
+
+
+echo -en "hola $NOMBRE, este es su correo [${GREEN}$EMAI${RESET}]? [s/n]: "
+read RESP
+
+if [[ s == $RESP ]]
+
+then
+
+	echo hola $NOMBRE, su correo es $EMAIL
+
+fi
 
 
